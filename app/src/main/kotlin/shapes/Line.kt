96a39d869ea2a97f8c49341bac6f561f81660e87
sackofcookies <tommy.lesueur.temp@outlook.com>
2025-06-26 17:53:43 -0600

    created Line class

---
+++ app/src/main/kotlin/shapes/Line.kt
@@ -0,0 +1,17 @@
+import kotlin.math.sqrt
+import kotlin.math.pow
+
+class Line(p1: Point, p2: Point): Shape{
+    var p1: Point = p1
+        get() = p1.clone()
+    var p2: Point = p2
+        get() = p2.clone()
+
+    override public fun move(deltaX: Double, deltaY: Double){
+        p1.move(deltaX, deltaY)
+        p2.move(deltaX, deltaY)
+    }
+
+    public fun getSlope(): Double = ((p2.y - p1.y)/(p2.x - p1.x))
+    public fun getLength(): Double = sqrt(((p2.x - p1.x)*(p2.x - p1.x))+((p2.y - p1.y)-(p2.y - p1.y)))
+}
