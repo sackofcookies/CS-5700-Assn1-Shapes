1236426dae4ecbfe778266cf60434194b3848fde
sackofcookies <tommy.lesueur.temp@outlook.com>
2025-06-30 09:07:04 -0600

    added error checking to point

--- app/src/main/kotlin/shapes/Point.kt
+++ app/src/main/kotlin/shapes/Point.kt
@@ -10,4 +10,16 @@ class Point(x: Double, y: Double) : Shape{
         x += deltaX
         y += deltaY
     }
+
+    init {
+        if (x == Double.POSITIVE_INFINITY || x == Double.NEGATIVE_INFINITY){
+            throw IllegalArgumentException("x cannot be infinity")
+        }
+        if (y == Double.POSITIVE_INFINITY || y == Double.NEGATIVE_INFINITY){
+            throw IllegalArgumentException("y cannot be infinity")
+        }
+        if (x == Double.NaN || y == Double.NaN){
+            throw IllegalArgumentException("Values cannot be NaN")
+        }
+    }
 }
