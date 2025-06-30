f68e46278a2bfa0971e07c51c2fe83732d7485ab
sackofcookies <tommy.lesueur.temp@outlook.com>
2025-06-30 09:32:49 -0600

    created Square class

---
+++ app/src/main/kotlin/shapes/Square.kt
@@ -0,0 +1,2 @@
+//p1 is the bottom left point if sideLength is positive and is the top right point if sideLength is negative
+class Square(p1: Point, sideLength: Double): Rectangle(p1, Point(p1.x + sideLength, p1.y + sideLength))
